dda1bcce43f7e56f88553135de46fc1d4244cf42
Muhammad Ahmad <muhammadahmad@zeepalm.com>
2024-06-25 05:51:09 -0700

    model is buult

---
+++ ai-photo-editor/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import PhotoEditor from "./Components/PhotoEditor";
+
+function App() {
+  return (
+    <>
+      <PhotoEditor />
+    </>
+  );
+}
+
+export default App;
---
+++ ai-photo-editor/src/Components/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PhotoEditor from "./PhotoEditor";
+
+export default function index() {
+  return (
+    <div>
+      <PhotoEditor />
+    </div>
+  );
+}
